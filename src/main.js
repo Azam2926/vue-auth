28be704e3a5f0415134da040e92597dd8e652147
Azam2926 <azam.inoyatov.1998@gmail.com>
2020-11-16 17:31:48 +0500

    installed vue-router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import Axios from 'axios'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ if (token)
 
 new Vue({
   store,
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
