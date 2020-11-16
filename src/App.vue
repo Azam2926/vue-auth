a144729179df90f6e7a1644b52167fd9f31219d5
Azam2926 <azam.inoyatov.1998@gmail.com>
2020-11-16 20:55:22 +0500

    whenever user token invalid or does not exist we added axios interceptors user to go login page

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,18 @@ export default {
       })
     },
   },
+
+  created: function () {
+    this.$http.interceptors.response.use(undefined, function (err) {
+      // eslint-disable-next-line no-unused-vars
+      return new Promise(function (resolve, reject) {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch('logout')
+        }
+        throw err
+      })
+    })
+  },
 }
 </script>
 
